6b578817e5d81913304878c0a332f1dfb83318f1
Gustavo Costa <cmtehenz@gmail.com>
2018-02-26 16:10:49 -0300

    default value

--- course/js/main.js
+++ course/js/main.js
@@ -71,3 +71,19 @@ var age = 30;
 var text = "\n    name: " + name + "\n    age: " + age + "\n";
 
 console.log(text);
+/*
+function test(valueA){
+    var valueB = arguments.length <= 1 || arguments[1] === undefined ? 4 : arguments[1]
+    return valueA + valueB;
+}
+
+console.log(test(2));
+*/
+
+function test(valueA) {
+    var valueB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 4;
+
+    return valueA + valueB;
+}
+
+console.log(test(4));
--- course/src/app.js
+++ course/src/app.js
@@ -55,6 +55,19 @@ let text = `
     age: ${age}
 `;
 
-
 console.log(text);
+/*
+function test(valueA){
+    var valueB = arguments.length <= 1 || arguments[1] === undefined ? 4 : arguments[1]
+    return valueA + valueB;
+}
+
+console.log(test(2));
+*/
+
+function test(valueA, valueB = 4){
+    return valueA + valueB;
+}
+
+console.log(test(4));
 
